2d8ff292924f6171d3106ba7da81e0ae9a8c9e12
Jos van Bakel <jos@codeaddict.org>
2021-04-25 12:42:09 +0200

    udp.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
 #include <espbase.h>
 #include "display.h"
 #include "text.h"
-
-WiFiUDP udpSocket;
-byte packetBuffer[UDP_MAX_PACKET_SIZE];
-
-void handleUdp();
+#include "udp.h"
 
 void setup(void) {
     Serial.begin(115200);
@@ -14,27 +10,11 @@ void setup(void) {
 
     setupDisplay();
     setupEspbase();
-
-    if (udpSocket.begin(UDP_PORT) != 1) {
-        Serial.printf("Failed to start UDP socket on port %d\n\r", UDP_PORT);
-    }
+    setupUdp();
 }
 
 void loop() {
     handleEspbase();
     handleUdp();
     textLoop();
-}
-
-void handleUdp() {
-    int packetSize = udpSocket.parsePacket();
-    if (packetSize) {
-        int len = udpSocket.read(packetBuffer, sizeof(packetBuffer));
-        if (len == LED_WIDTH*LED_HEIGHT/8) {
-            if (resetText()) {
-                u8g2.drawXBM(0, 0, LED_WIDTH, LED_HEIGHT, packetBuffer);
-                u8g2.sendBuffer();
-            }
-        }
-    }
 }
--- src/udp.cpp
+++ src/udp.cpp
@@ -1,2 +1,26 @@
-#include "espbase.h"
+#include <espbase.h>
+#include "display.h"
+#include "text.h"
+
+WiFiUDP udpSocket;
+byte packetBuffer[UDP_MAX_PACKET_SIZE];
+
+void setupUdp() {
+    if (udpSocket.begin(UDP_PORT) != 1) {
+        Serial.printf("Failed to start UDP socket on port %d\n\r", UDP_PORT);
+    }
+}
+
+void handleUdp() {
+    int packetSize = udpSocket.parsePacket();
+    if (packetSize) {
+        int len = udpSocket.read(packetBuffer, sizeof(packetBuffer));
+        if (len == LED_WIDTH*LED_HEIGHT/8) {
+            if (resetText()) {
+                u8g2.drawXBM(0, 0, LED_WIDTH, LED_HEIGHT, packetBuffer);
+                u8g2.sendBuffer();
+            }
+        }
+    }
+}
 
